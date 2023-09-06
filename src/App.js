b0085812e2d34d29c6efc7fbac911664b892174a
NourhanGamal7 <aoo446085@gmail.com>
2023-09-06 06:03:07 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import NavIcon from "./components/NavIcon";
+import Recipe from "./components/Recipe";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-poppins bg-back">
+      <Header />
+      <NavIcon />
+      <Home />
+      <About />
+      <Recipe />
+      <Contact />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
